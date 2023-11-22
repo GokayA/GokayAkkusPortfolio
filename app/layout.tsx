47001686f827fedcb110068c0ab55a2039fc7415
GokayA <arjenrobbennn@gmail.com>
2023-11-22 15:28:25 +0300

    antialized update

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <body className={font.className}>
         <Header />
         {children}
