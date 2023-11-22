94f4d167a6fe9eb330a90c2613e571f040e8ac9e
GokayA <arjenrobbennn@gmail.com>
2023-11-22 15:40:34 +0300

    overflow fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="antialiased">
-      <body className={font.className}>
+    <html lang="en" className="h-full ">
+      <body className={`${font.className} antialiased h-full relative`}>
         <Header />
-        {children}
+        <main className="relative flex flex-col min-h-screen">
+          <div className="flex-grow flex-1">{children}</div>
+        </main>
         <Analytics />
       </body>
     </html>
